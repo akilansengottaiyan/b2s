45ea59b6ec1bf6e43986ddefb8d47c63a4e04f1b
I351484 <akilan0306@gmail.com>
2018-09-25 16:10:44 +0530

    optimised and organised authoriser.js

--- utilities/authoriser.js
+++ utilities/authoriser.js
@@ -1,37 +1,38 @@
 var jwt = require('jsonwebtoken');
+var jwtSecret ;
 
-var authoriseAdmin = function(req,res,next){
+var commonAuth = function(req,res,next){
     var token = req.body.token || req.header('x-access-token');
     if(token){
-        var jwtSecret = process.env.ADMIN_TOKEN_SECRET;
-     jwt.verify(token, jwtSecret).then( decoded =>{
-         req.decoded = decoded;
-         next();
+     new Promise((resolve,reject) => {
+        jwt.verify(token, jwtSecret, function(err ,decoded ){
+            if(err){
+                reject(err);
+            }
+            else {
+            resolve(decoded);
+            }
+        })
+     }).then(decoded => {
+        req.decoded = decoded;
+        next();
      }).catch( err => {
-         res.sendStatus('500');
-    });
+        sendResponse('401',{auth:false, message:"Token Verification Failed. "+err});
+     })
     }
     else{
-        res.status('401').send({auth:false, message:"Invalid token"});
+        sendResponse('401',{auth:false, message:"Token not found."});
     }
 }
 
+var authoriseAdmin = function(req,res,next){
+    jwtSecret = process.env.ADMIN_TOKEN_SECRET;
+    commonAuth(req,res,next);
+}
+
 var authoriseUser = function(req,res,next){
-    var token = req.body.token || req.header('x-access-token');
-    if(token){
-        var jwtSecret = process.env.USER_TOKEN_SECRET;
-     jwt.verify(token, jwtSecret, function(err ,decoded ){
-         if(err){
-            res.status('500').send('Token verification failed.');
-         }
-         req.decoded = decoded;
-         console.log(decoded);
-         next();
-     });
-    }
-    else{
-        res.status('401').send({auth:false, message:"Token not found."});
-    }
+    jwtSecret = process.env.USER_TOKEN_SECRET;
+    commonAuth(req,res,next);
 }
 
 module.exports.authoriseUser = authoriseUser ;
